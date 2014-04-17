c3c7764e11f4e85e5e583f4b62ee4ab7209fb4ca
y-ken <y.ken.studio@gmail.com>
2014-04-17 15:51:10 +0900

    Add line spaces

--- provision.sh
+++ provision.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -e # exit on error
 
+
 # Configuration
 INSTALL_RUBY_VERSIONS=("1.9.3-p545" "2.1.1")
 PROFILE=~/.bash_profile
 
+
 # Install build required package for rbenv
+## For another operating system, Refer the page below.
+## https://github.com/sstephenson/ruby-build/wiki#suggested-build-environment
 yum -y install gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel git
+
 
 # Install build required package for Apache Passenger
 yum -y install curl-devel httpd-devel
+
 
 # Install rbenv
 cd /root
@@ -37,20 +43,26 @@ for version in ${INSTALL_RUBY_VERSIONS[@]}; do
   passenger-install-apache2-module --languages ruby --auto 2>/dev/null
 done
 
+
 # Set executable option
 chmod o+x /root
 chmod o+x /root/.rbenv/
 
+
 # Put configuration files into apache
 cp /vagrant/passenger.conf /etc/httpd/conf.d/passenger.conf
 cp /vagrant/virtualhosts.conf /etc/httpd/conf.d/virtualhosts.conf
+
 
 # Put sample applications
 for version in "ruby19" "ruby21"; do
   cp -rp /vagrant/$version.example.jp /var/www/$version.example.jp
 done
 
+
 # Start apache service
 service httpd start
 
+
 echo "Done!!"
+
